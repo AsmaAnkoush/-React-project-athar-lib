4ec810f37201ae2b7d3e5d9a34bc6f9d267f2f20
unknown <asmaa.ankoush2003@gmail.com>
2025-09-18 14:25:51 -0700

    Add Netlify redirect for SPA

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import MaterialsPage from './pages/SubjectList';
+import LabsPage from './pages/LabList';
+import FeedbackPrompt from "./components/FeedbackPrompt";
+
 
 function App() {
     return (
+        <BrowserRouter>
+            <main className="bg-[#e0e7ff] min-h-screen text-gray-900">
+                <Routes>
+                    <Route path="/" element={<Hero />} />
+                    <Route path="/subjects" element={<MaterialsPage />} />
+                    <Route path="/labs" element={<LabsPage />} />
+                </Routes>
+            </main>
+                  <FeedbackPrompt />
 
-            <BrowserRouter>
-                <main className="bg-[#e0e7ff] min-h-screen text-gray-900">
-
-                    <Routes>
-                        <Route path="/" element={<Hero />} />
-                        <Route path="/subjects" element={<MaterialsPage />} />
-                    </Routes>
-                </main>
-            </BrowserRouter>
-
+        </BrowserRouter>
     );
 }
 
